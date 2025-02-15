e802af8b273f4b3b347e0bc627f2c575e6fe07f0
mhvuong <mhvuong00@gmail.com>
2025-02-15 17:50:01 -0600

    Add attendance vs. flood counts plot

--- plots.R
+++ plots.R
@@ -20,9 +20,21 @@ p1 <- ggplot(data, aes(x = average_precipitation, y = attendance)) +
 ggsave(filename = paste0(plot_path, "/attendance_vs_precipitation.png"),
        plot = p1, width = 10, height = 6, dpi = 300)
 
+# Scatter Plot of Attendance vs. Count of Flood Incidents
+p2 <- ggplot(data, aes(x = flood_incidents, y = attendance)) +
+  geom_point(alpha = 0.5) +  
+  geom_smooth(method = "lm", color = "royalblue4", fill = "royalblue4", se = FALSE) +
+  labs(title = "Attendance vs. Precipitation",
+       x = "Precipitation (mm)",
+       y = "Attendance Rate (%)") +
+  theme_minimal() +
+  theme(plot.background = element_rect(fill = "white"))
+
+ggsave(filename = paste0(plot_path, "/attendance_vs_flood_counts.png"),
+       plot = p1, width = 10, height = 6, dpi = 300)
 
 # Bar Chart of Average Attendance by County
-p2 <- ggplot(data, aes(x = county, y = attendance)) +
+p3 <- ggplot(data, aes(x = county, y = attendance)) +
   geom_bar(stat = "summary", fun = "mean", fill = "steelblue") +
   labs(title = "Average Attendance by County",
        x = "County",
@@ -33,7 +45,7 @@ p2 <- ggplot(data, aes(x = county, y = attendance)) +
 ggsave(filename = paste0(plot_path, "/average_attendance_by_county.png"), plot = p2, width = 10, height = 6, dpi = 300)
 
 # Line Graph of Attendance Over Time
-p3 <- ggplot(data, aes(x = school_year, y = attendance, group = county, color = county)) +
+p4 <- ggplot(data, aes(x = school_year, y = attendance, group = county, color = county)) +
   geom_line() +
   scale_x_discrete(breaks = function(x) unique(x)[c(TRUE, rep(FALSE, 1))]) + # display every second year
   labs(title = "Attendance Over Time by County",
